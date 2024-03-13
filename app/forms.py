af3a1197925c1e8f22b115d8ed76c666fa215397
Dzoni <160143382+Dzoni85IFA@users.noreply.github.com>
2024-03-13 23:15:15 +0100

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -32,6 +32,7 @@ class RegistrationForm(FlaskForm):
 class ToDoForm(FlaskForm):
     titel = StringField('Titel', validators=[DataRequired()])
     description = TextAreaField('Beschreibung')
-    status = StringField('Status', validators=[DataRequired()])
+    status_choices = [('Neu'), ('In Arbeit'), ('Erledigt')]
+    status = SelectField('Status', choices=status_choices, validators=[DataRequired()])
     todountil = DateField('Fälligkeitsdatum', validators=[DataRequired()])
     submit = SubmitField('Speichern')
