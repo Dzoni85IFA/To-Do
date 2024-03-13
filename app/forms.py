d8ac5b76ba20655aa1cd4310c81d2ee8d5078efa
Dzoni <160143382+Dzoni85IFA@users.noreply.github.com>
2024-03-13 23:26:06 +0100

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, DateField, TextAreaField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, DateField, TextAreaField, SelectField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 from app.models import User
 
