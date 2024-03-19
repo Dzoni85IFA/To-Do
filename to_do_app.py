0a12f9dadef53353be2bd70ca53f589e9fd29fc7
Dzoni <160143382+Dzoni85IFA@users.noreply.github.com>
2024-03-19 11:09:13 +0100

    Update to_do_app.py

--- to_do_app.py
+++ to_do_app.py
@@ -1,7 +1,7 @@
 from app import app, db
 from app.models import User, ToDo
 
-
+# Funktion definieren, um Kontext für das Python-Shell-Environment bereitzustellen
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'ToDo': ToDo}
