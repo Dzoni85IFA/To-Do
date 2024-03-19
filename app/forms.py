620ac40419d07bd54a4508e495aff42d9ef37235
Dzoni <160143382+Dzoni85IFA@users.noreply.github.com>
2024-03-19 11:07:36 +0100

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -3,14 +3,14 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField, DateF
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 from app.models import User
 
-
+#Definiert ein Anmeldeformular mit Feldern für Benutzername, Passwort und "Remember Me"
 class LoginForm(FlaskForm):
     username = StringField('Benutzername', validators=[DataRequired()])
     password = PasswordField('Passwort', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Anmeldung')
 
-
+# Definiert ein Registrierungsformular mit Feldern für Benutzername, E-Mail, Passwort und Passwortbestätigung
 class RegistrationForm(FlaskForm):
     username = StringField('Benutzername', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
@@ -18,17 +18,20 @@ class RegistrationForm(FlaskForm):
     password2 = PasswordField(
         'Passwort wiederholen', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Registrieren')
-
+    
+    # Überprüft, ob der Benutzername bereits verwendet wird
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
             raise ValidationError('Bitte verwenden Sie einen anderen Benutzernamen!')
-
+    
+    # Überprüft, ob die E-Mail-Adresse bereits verwendet wird
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Bitte verwenden Sie eine andere E-Mail Adresse!')
 
+# Definiert ein Formular für das Erstellen eines To-Do-Eintrags mit Titel, Beschreibung, Status und Fälligkeitsdatum Feldern
 class ToDoForm(FlaskForm):
     titel = StringField('Titel', validators=[DataRequired()])
     description = TextAreaField('Beschreibung', validators=[DataRequired()])
