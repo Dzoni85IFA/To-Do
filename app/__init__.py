ab6249966912a39ee9a16de5832cea73c7788f17
Dzoni <160143382+Dzoni85IFA@users.noreply.github.com>
2024-03-19 11:07:16 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,15 +6,20 @@ from config import Config
 
 
 
-app = Flask(__name__)
-app.config.from_object(Config)
+
+app = Flask(__name__) # Flask-App initialisieren
+app.config.from_object(Config) # Konfiguration der App aus der Config-Klasse laden
+
+# SQLAlchemy-Datenbankobjekt und Flask-Migrate initialisieren
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-login = LoginManager(app)
-login.login_view = 'login'
 
-from app import routes, models
+login = LoginManager(app) # Flask-Login für Benutzerauthentifizierung initialisieren
+login.login_view = 'login'  # Ansicht für den Login festlegen
 
+from app import routes, models # Routen und Modelle importieren
+
+# Benutzer laden, der mit der übergebenen user_id verknüpft ist
 @login.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
