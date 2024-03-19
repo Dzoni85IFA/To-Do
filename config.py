865b072c4fc0ab16dc49c732ca282fa1ec24ccb3
Dzoni <160143382+Dzoni85IFA@users.noreply.github.com>
2024-03-19 11:08:44 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
+# Basisverzeichnis des Projekts bestimmen
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
+# Konfigurationsklasse definieren
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
